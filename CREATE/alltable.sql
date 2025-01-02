b2b7d715781aa2e2c6792828c16bb40471876a4b
Danzyyyy <tubagus.233040099@mail.unpas.ac.id>
2025-01-02 22:59:47 +0700

    edit transaksi_keuangan && laporan_pajak_detail

--- CREATE/alltable.sql
+++ CREATE/alltable.sql
@@ -29,7 +29,6 @@ CREATE TABLE transaksi_keuangan (
 	wajib_pajak_id BIGINT NOT NULL,
 	tanggal_transaksi DATE NOT NULL,
 	nominal BIGINT NOT NULL,
-	nama VARCHAR(255) NOT NULL,
 	keterangan VARCHAR(255) NOT NULL,
 	jenis_transaksi TINYINT DEFAULT 0,
 	FOREIGN KEY (wajib_pajak_id) REFERENCES wajib_pajak(id)
@@ -49,6 +48,7 @@ CREATE TABLE laporan_pajak_detail (
 	id BIGINT IDENTITY(1,1) PRIMARY KEY,
 	laporan_pajak_id BIGINT NOT NULL,
 	kategori_pajak_id BIGINT NOT NULL,
+	jumlah_transaksi INT NOT NULL,
 	nominal_pajak DECIMAL(10,2) NOT NULL,
 	FOREIGN KEY (laporan_pajak_id) REFERENCES laporan_pajak(id)
 		ON DELETE CASCADE
