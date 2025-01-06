6fa27fd4fcc5a6d6207213f293c5539e501db0c5
Danzyyyy <tubagus.233040099@mail.unpas.ac.id>
2025-01-06 23:34:43 +0700

    edit

--- CREATE/alltable.sql
+++ CREATE/alltable.sql
@@ -2,8 +2,8 @@
 CREATE TABLE golongan (
     id BIGINT IDENTITY(1,1) PRIMARY KEY,
     nama VARCHAR(255) NOT NULL,
-    created_at DATETIME NOT NULL,
-    updated_at TIMESTAMP NOT NULL
+    created_at DATETIME DEFAULT GETDATE(),
+    updated_at DATETIME DEFAULT GETDATE()
 );
 
 -- Tabel tarif_efektif_rata_rata
@@ -12,8 +12,8 @@ CREATE TABLE tarif_efektif_rata_rata (
     jumlah_minimal DECIMAL(18, 2) NOT NULL,
     jumlah_maksimal DECIMAL(18, 2) NOT NULL,
     persentase_pajak FLOAT NOT NULL,
-    created_at DATETIME NOT NULL,
-    updated_at TIMESTAMP NOT NULL
+    created_at DATETIME DEFAULT GETDATE(),
+    updated_at DATETIME DEFAULT GETDATE()
 );
 
 -- Tabel tarif_efektif_per_golongan untuk hubungan ke Tabel golongan dan tarif_efektif_rata_rata
@@ -21,8 +21,8 @@ CREATE TABLE tarif_efektif_per_golongan (
     id BIGINT IDENTITY(1,1) PRIMARY KEY,
     golongan_id BIGINT NOT NULL,
     tarif_id BIGINT NOT NULL,
-    created_at DATETIME NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
+    created_at DATETIME DEFAULT GETDATE(),
+    updated_at DATETIME DEFAULT GETDATE(),
 	FOREIGN KEY (golongan_id) REFERENCES golongan(id)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE,
@@ -36,8 +36,8 @@ CREATE TABLE ptkp (
     id BIGINT IDENTITY(1,1) PRIMARY KEY,
     nama VARCHAR(255) NOT NULL,
     jumlah DECIMAL(18, 2) NOT NULL,
-    created_at DATETIME NOT NULL,
-    updated_at TIMESTAMP NOT NULL
+    created_at DATETIME DEFAULT GETDATE(),
+    updated_at DATETIME DEFAULT GETDATE()
 );
 
 --Tabel wajib_pajak untuk hubungan ke Tabel golongan dan ptkp
@@ -51,8 +51,8 @@ CREATE TABLE wajib_pajak (
     tipe_wajib_pajak TINYINT DEFAULT 0,
     golongan_id BIGINT NOT NULL,
     ptkp_id BIGINT NOT NULL,
-    created_at DATETIME NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
+    created_at DATETIME DEFAULT GETDATE(),
+    updated_at DATETIME DEFAULT GETDATE(),
 	FOREIGN KEY (golongan_id) REFERENCES golongan(id)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE,
@@ -68,8 +68,8 @@ CREATE TABLE laporan_penghasilan (
     tanggal_transaksi DATE NOT NULL,
     nominal BIGINT NOT NULL,
     keterangan VARCHAR(255) NOT NULL,
-    created_at DATETIME NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
+    created_at DATETIME DEFAULT GETDATE(),
+    updated_at DATETIME DEFAULT GETDATE(),
 	FOREIGN KEY (wajib_pajak_id) REFERENCES wajib_pajak(id)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE
@@ -86,8 +86,8 @@ CREATE TABLE akumulasi_penghasilan_bulanan (
     total_pajak DECIMAL(18, 2) NOT NULL,
     penghasilan_bersih DECIMAL(18, 2) NOT NULL,
     periode DATE NOT NULL,
-    created_at DATETIME NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
+    created_at DATETIME DEFAULT GETDATE(),
+    updated_at DATETIME DEFAULT GETDATE(),
 	FOREIGN KEY (wajib_pajak_id) REFERENCES wajib_pajak(id)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE,
@@ -104,8 +104,8 @@ CREATE TABLE akumulasi_penghasilan_tahunan (
     selisih_pajak DECIMAL(18, 2) NOT NULL,
     tahun INT NOT NULL,
     jumlah_ptkp DECIMAL(18, 2) NOT NULL,
-    created_at DATETIME NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
+    created_at DATETIME DEFAULT GETDATE(),
+    updated_at DATETIME DEFAULT GETDATE(),
 	FOREIGN KEY (wajib_pajak_id) REFERENCES wajib_pajak(id)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE
@@ -118,8 +118,8 @@ CREATE TABLE potongan_pajak_tahunan (
     persentase_pajak FLOAT NOT NULL,
     penghasilan_kena_pajak DECIMAL(18, 2) NOT NULL,
     total_pajak DECIMAL(18, 2) NOT NULL,
-    created_at DATETIME NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
+    created_at DATETIME DEFAULT GETDATE(),
+    updated_at DATETIME DEFAULT GETDATE(),
 	FOREIGN KEY (akumulasi_penghasilan_tahunan_id) REFERENCES akumulasi_penghasilan_tahunan(id)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE
@@ -130,8 +130,8 @@ CREATE TABLE akumulasi_bulanan_tahunan (
     id BIGINT IDENTITY(1,1) PRIMARY KEY,
     akumulasi_penghasilan_bulanan_id BIGINT NOT NULL,
     akumulasi_penghasilan_tahunan_id BIGINT NOT NULL,
-    created_at DATETIME NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
+    created_at DATETIME DEFAULT GETDATE(),
+    updated_at DATETIME DEFAULT GETDATE(),
 	FOREIGN KEY (akumulasi_penghasilan_bulanan_id) REFERENCES akumulasi_penghasilan_bulanan(id)
 		ON DELETE NO ACTION
 		ON UPDATE NO ACTION,
@@ -145,8 +145,8 @@ CREATE TABLE akumulasi_potongan_tahunan (
     id BIGINT IDENTITY(1,1) PRIMARY KEY,
     akumulasi_penghasilan_tahunan_id BIGINT NOT NULL,
     potongan_pajak_tahunan_id BIGINT NOT NULL,
-    created_at DATETIME NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
+    created_at DATETIME DEFAULT GETDATE(),
+    updated_at DATETIME DEFAULT GETDATE(),
 	FOREIGN KEY (akumulasi_penghasilan_tahunan_id) REFERENCES akumulasi_penghasilan_tahunan(id)
 		ON DELETE NO ACTION
 		ON UPDATE NO ACTION,
@@ -161,8 +161,8 @@ CREATE TABLE pengguna (
     nama VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     passwords VARCHAR(255) NOT NULL,
-    created_at DATETIME NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
+    created_at DATETIME DEFAULT GETDATE(),
+    updated_at DATETIME DEFAULT GETDATE(),
 );
 
 --Tabel rekapitulasi_laporan untuk hubungan ke Tabel akumulasi_penghasilan_bulanan dan laporan_penghasilan
@@ -170,8 +170,8 @@ CREATE TABLE laporan_penghasilan_akumulasi_bulanan (
     id BIGINT IDENTITY(1,1) PRIMARY KEY,
     akumulasi_penghasilan_bulanan_id BIGINT NOT NULL,
     laporan_penghasilan_id BIGINT NOT NULL,
-    created_at DATETIME NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
+    created_at DATETIME DEFAULT GETDATE(),
+    updated_at DATETIME DEFAULT GETDATE(),
 	FOREIGN KEY (akumulasi_penghasilan_bulanan_id) REFERENCES akumulasi_penghasilan_bulanan(id)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE,
--- INSERT/golongan.sql
+++ INSERT/golongan.sql
@@ -1,5 +1,5 @@
 -- Insert data golongan
-INSERT INTO golongan (nama, created_at) VALUES
-('Golongan A', '2023-01-01 08:00:00'),
-('Golongan B', '2023-01-02 09:00:00'),
-('Golongan C', '2023-01-03 10:00:00');
+INSERT INTO golongan (nama, created_at, updated_at) VALUES
+('Golongan A', '2023-01-01 08:00:00', '2023-01-01 08:00:00'),
+('Golongan B', '2023-01-02 09:00:00', '2023-01-02 09:00:00'),
+('Golongan C', '2023-01-03 10:00:00', '2023-01-03 10:00:00');
